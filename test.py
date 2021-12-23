bfc7b6ee3921ee80a15f8e096455d2c5944902ba
lmart <lmartini@live.com>
2021-12-23 13:37:49 -0800

    reorg

--- test.py
+++
@@ -1,3 +0,0 @@
-print("testing testing")
-edit = str(input("Enter your fav number here: "))
-print(edit)
--- update_main_playlists.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python3 download_youtube.py 'https://www.youtube.com/playlist?list=PL5u9rXq186GWIEg2k5fcR_EHljxpStpjy' mp3 /mnt/c/Users/lmart/Music/downloads/general_music
-python3 download_youtube.py 'https://www.youtube.com/playlist?list=PL5u9rXq186GUzfr3O64K25hDOnqQ3dmEd' mp3 /mnt/c/Users/lmart/Music/downloads/easy_listenin
