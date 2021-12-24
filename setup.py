5a302a3d3d3b17a100e76b98e78552b05d6f98a7
lmart <lmartini@live.com>
2021-12-23 17:21:17 -0800

    pre upload attempt

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ setup(
         name="yt-downloader",
         version="0.1.0",
         packages=["yt-downloader"],
+        license="MIT",
+        description="A wrapper for pytube3 that makes it easier to download videos and playlists as mp3 or mp4.",
+        url="https://github.com/lauren-martini/yt-downloader",
         install_requires=[
             'python_version >= 3',
-            'pytube',
+            'pytube3',
             'tqdm',
             'ffmpeg',
         ],
