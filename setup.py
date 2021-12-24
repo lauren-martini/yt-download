e74f9cb6ebe160a046d986bc9a9e712132923665
lmart <lmartini@live.com>
2021-12-24 00:54:36 -0800

    Massive messy reorg

--- setup.py
+++ setup.py
@@ -2,15 +2,21 @@ from setuptools import setup
 
 setup(
         name="yt-download",
-        version="0.1.0",
-        packages=["yt-download"],
+        version="0.1.1",
+        packages=["yt_download"], # containing folder for yt_downloader.py
         license="MIT",
         description="A wrapper for pytube3 that makes it easier to download videos and playlists as mp3 or mp4.",
         url="https://github.com/lauren-martini/yt-download",
+        py_modules=['yt_download'], # module is yt_download.py
+        author="lauren-martini",
         install_requires=[
-            'python_version >= 3',
             'pytube3',
             'tqdm',
             'ffmpeg',
         ],
+        entry_points={
+            'console_scripts': [
+                'yt-download = yt_download.yt_download:main',
+            ],
+        },
 )
